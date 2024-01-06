b0a101c49516d0515779448dca417fd0716ff76e
Chimobi Ekwunife <ekwunifechimobi2004@gmail.com>
2024-01-06 11:50:15 -0800

    to upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,9 +6,14 @@ def uppercase(str):
     uppercase: converts lowercase characters to uppercase
     :str: string or phrase passed in
     '''
+    upper = []
 
     for st in str:
         if ord(st) in list(range(97, 123)):
-            print("{}".format(chr(ord(st) - 32)), end='')
+            upper.append(chr(ord(st) - 32))
         else:
-            print("{}".format(st), end='')
+            upper.append(st)
+
+    for up in upper:
+        print("{}".format(up), end='')
+    print()
