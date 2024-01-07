39ca2b382e43bf323afd7f08ab0bfef79a5c83c6
Chimobi Ekwunife <ekwunifechimobi2004@gmail.com>
2024-01-07 03:36:14 -0800

    reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,5 +3,5 @@
 i = 0
 
 for ch in list(range(ord('z'), ord('a') - 1, -1)):
-        print("{}".format(chr(ch - i)), end='')
-        i = 32 if i == 0 else 0
+    print("{}".format(chr(ch - i)), end='')
+    i = 32 if i == 0 else 0
