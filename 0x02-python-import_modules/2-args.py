cfb06ccb6c07f73c78512e8a3ca9861e4037f64c
Ekwunife Chimobi <100740932+mrfantastic16@users.noreply.github.com>
2024-01-09 09:13:39 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
-    import sys
+    from sys import argv
 
     j = 0
-    for i in range(1, len(sys.argv)):
+    for i in range(1, len(argv)):
         j += 1
     
     if j == 0:
-        print("{} arguements.".format(j))
+        print(f"{j} arguements.")
     elif j == 1:
-        print(f"{j} arguement:\n{j}: {sys.argv[1]}")
+        print(f"{j} arguement:\n{j}: {argv[1]}")
     else:
         print(f"{j} arguement:")
         for i in range(1, j+1):
-            print(f"{i}: {sys.argv[i]}")
+            print(f"{i}: {argv[i]}")
