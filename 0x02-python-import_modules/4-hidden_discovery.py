56bf9f689f51215617c64303019fbc6e66b14e7b
Ekwunife Chimobi <100740932+mrfantastic16@users.noreply.github.com>
2024-01-09 11:30:33 +0000

    hidden discovery

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    import hidden_4
+
+    lst = []
+
+    for names in hidden_4.__dict__:
+        if not (names.startswith('__')):
+            lst.append(names)
+
+    lst.sort()
+
+    for name in lst:
+        print(name)
