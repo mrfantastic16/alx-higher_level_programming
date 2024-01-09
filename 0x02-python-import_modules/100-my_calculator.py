646b0aa8f742d19eec5b5f3a77137c6b8f69612e
Ekwunife Chimobi <100740932+mrfantastic16@users.noreply.github.com>
2024-01-09 13:02:26 +0000

    calc-more-func

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+def main():
+    if __name__ == '__main__':
+        from calculator_1 import add, sub, mul, div
+        from sys import argv, exit
+
+        a_operators = {
+            "+": add,
+            "-": sub,
+            "*": mul,
+            "/": div
+        }
+
+        if len(argv) != 4:
+            print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+            exit(1)
+
+        operand_1 = int(argv[1])
+        operand_2 = int(argv[3])
+        op = check_op(argv[2])
+
+        result = a_operators[op](operand_1, operand_2)
+        print(result)
+
+
+def check_op(op):
+    if op not in ["+", "-", "*", "/"]:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        return op
+
+
+main()
