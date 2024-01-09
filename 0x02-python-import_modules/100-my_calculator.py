0e2b598812d812feb26f2fd9a95015acdd779586
Ekwunife Chimobi <100740932+mrfantastic16@users.noreply.github.com>
2024-01-09 13:59:03 +0000

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
 
-if __name__ == '__main__':
-    from calculator_1 import add, sub, mul, div
-    from sys import argv, exit
+def main():
+    if __name__ == '__main__':
+        from calculator_1 import add, sub, mul, div
+        from sys import argv, exit
 
-    a_operators = {
-        "+": add,
-        "-": sub,
-        "*": mul,
-        "/": div
-        }
+        a_operators = {
+            "+": add,
+            "-": sub,
+            "*": mul,
+            "/": div
+            }
 
-    if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        if len(argv) != 4:
+            print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+            exit(1)
 
-    operand_1 = int(argv[1])
-    operand_2 = int(argv[3])
-    op = (argv[2])
+        operand_1 = int(argv[1])
+        operand_2 = int(argv[3])
+        op = (argv[2])
 
-    result = a_operators[op](operand_1, operand_2)
-    print(f"{operand_1} {op} {operand_2} = {result}")
+        result = a_operators[op](operand_1, operand_2)
+        print(f"{operand_1} {op} {operand_2} = {result}")
 
 
 def check_op(op):
     if op not in ["+", "-", "*", "/"]:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-
-    result = a_operators[op](operand_1, operand_2)
-    print(result)
 
 
 main()
