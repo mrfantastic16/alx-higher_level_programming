2d43fa466727c85f271b0280a918c624a873395e
Ekwunife Chimobi <100740932+mrfantastic16@users.noreply.github.com>
2024-01-10 13:22:58 +0000

    nest-list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print("{:d}".format(num), end=' ')
-        print()
+            if num != row[len(row) - 1]:
+                print("{:d}".format(num), end=' ')
+            else:
+                print("{:d}".format(num))
