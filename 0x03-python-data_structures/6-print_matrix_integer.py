a8c606cc968057f1be02da508d646b682a233714
Ekwunife Chimobi <100740932+mrfantastic16@users.noreply.github.com>
2024-01-10 13:31:04 +0000

    nest-list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
     for row in matrix:
         for num in row:
             if num != row[len(row) - 1]:
