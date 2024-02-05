452e7613318a5b26504a1f4bb2ed03a18186457b
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-02-05 15:03:23 +0100

    square class with a getter method, setter method and a claculate area method

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+''' Define a Square Class '''
+
+
+class Square:
+    ''' A square template to
+    Args:
+        size (int): size of sqauere sizes
+    '''
+
+    def __init__(self, size):
+        '''Intializing a new square. '''
+        self.size = size
+
+    @property
+    def size(self):
+        '''Get the current size of square. '''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        '''Return the area of the sqaure'''
+        return (self.__size * self.__size)
