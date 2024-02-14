cc18c8d2b2e87eebdf1143454a2d6c320a1423c8
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-02-14 16:16:34 +0100

    doctstring that tests calculations

---
+++ 0x07-python-test_driven_development/real-python/calculations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """Compute and return the sum of two numbers.
+
+    Usage Examples:
+    >>> add(4.0, 2.0)
+    6.0
+    >>> add(4, 2)
+    6.0
+    """
+    return float(a + b)
