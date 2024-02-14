89341a41a82c9c5f50287c0d38abfe0b4534c9e6
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-02-14 16:27:29 +0100

    a python docstring used as to test a function output of strings

---
+++ 0x07-python-test_driven_development/real-python/printed_output.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def greet(name="World"):
+    """Print a greeting to the screen.
+
+    Usage examples:
+    >>> greet("Pythonista")
+    Hello, Pythonista!
+    >>> greet()
+    Hello, World!
+    """
+
+    print(f"Hello, {name}!")
