9ccaa64f9e83dfe5f5982d8a65678016bf673866
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-04-23 14:55:26 +0100

    task 2 soln, abs

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+ld = abs(number + (abs(number) % 10)) - abs(number)
+
+print(f"Last digit of {number: d} is {ld} and is", end=" ")
+if ld > 5:
+    print("greater than 5")
+elif ld == 0:
+    print("zero")
+else:
+    print("less than 6 and not 0")
