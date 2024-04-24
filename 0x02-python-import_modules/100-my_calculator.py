7d3491b5f5356a7904534771a0b835acec24a5e7
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-04-24 13:39:05 +0100

    task-6, my calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+import sys
+
+if len(sys.argv ) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
+
+ops = {"+": add, "-": sub, "*": mul, "/": div}
+op = sys.argv[2]
+
+if sys.argv[2] not in ops:
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
+
+a = int(sys.argv[1])
+b = int(sys.argv[3])
+
+print(f"{a} {op} {b} = {ops[op](a, b)}")
