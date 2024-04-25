5ff86da3e400057521d5b840553dd0d1cbee4c21
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-04-25 17:06:55 +0100

    corection

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-
-    for num in my_list:
-        print("{:d}".format(num))
+    if isinstance(my_list, list):
+        for m in my_list[::-1]:
+            print("{:d}".format(m))
