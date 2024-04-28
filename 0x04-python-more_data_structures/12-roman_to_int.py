f0876f6df42f7ec64830986c9b57a96b8d3e764a
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-04-28 16:14:09 +0100

    converting ints to roman numerals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,8 +5,26 @@ def roman_to_int(roman_string):
 		'I': 1,
 		'V': 5,
 		'X': 10,
-		'L': 100,
+		'L': 50,
+		'C': 100,
 		'D': 500,
 		'M': 1000
 	}
 
+	total = 0
+	prev_value = 0
+
+	if not isinstance(roman_string, str) or roman_string is None:
+		return 0
+
+	for chars in roman_string:
+		int_val = roman_int_map[chars]
+
+		if int_val > prev_value:
+			total += int_val - 2 * prev_value
+		else:
+			total += int_val
+
+		prev_value = int_val
+
+	return total
