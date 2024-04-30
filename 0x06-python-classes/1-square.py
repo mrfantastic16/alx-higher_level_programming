dc692e40d8b61275284e67f95ad1a0a52201296f
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-04-30 13:07:00 +0100

    task-1, intializing the square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Define a class Square. """
+
+
+class Square:
+    """ Represent a square. """
+
+    def __init__(self, size):
+        """Intialize the new Square"""
+        self.__size = size
