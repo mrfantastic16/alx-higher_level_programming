393dbace25a17bb72b5f01c2b25c64517054925a
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-05-07 09:14:22 +0100

    module contains say_my_name()

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+
+This module provides the say_my_name function
+
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    f"""Prints received parameters in the format
+    `My name is {first_name} {last_name}`
+    
+    Args:
+        first_name: param1 to be printed
+        last_name: optional param2
+
+    Returns: None
+
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
