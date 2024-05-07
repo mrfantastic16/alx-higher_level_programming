50a2e2b4c60a5ed6661694028e4a70501a66dda3
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-05-07 10:30:55 +0100

    test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# 6-max_integer_test.py
 """Unittests for max_integer([..])."""
 
 import unittest
@@ -45,18 +46,17 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_string(self):
         """Test a string."""
-        string = "Ehoneah"
+        string = "Brennan"
         self.assertEqual(max_integer(string), 'r')
 
     def test_list_of_strings(self):
         """Test a list of strings."""
-        strings = ["Ehoneah", "is", "my", "name"]
+        strings = ["Brennan", "is", "my", "name"]
         self.assertEqual(max_integer(strings), "name")
 
     def test_empty_string(self):
         """Test an empty string."""
         self.assertEqual(max_integer(""), None)
-
 
 if __name__ == '__main__':
     unittest.main()
